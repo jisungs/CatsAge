1f60ead0a1159643516526129d577c14cf144bc1
jisung <jisung.smart@gmail.com>
2019-03-14 08:55:06 +0900

    Practice of optional binding

--- CatsAge/ViewController.swift
+++ CatsAge/ViewController.swift
@@ -9,7 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func calculate(_ sender: Any) {
+        
+        if let age = textField.text {
+            if let ageAsNumber = Int(age){
+                let catsAge = ageAsNumber * 7
+                
+                label.text = "Your cat's age is " + String(catsAge) + "years old"
+            }
+        }
+        
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
